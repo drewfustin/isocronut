239ae43a48f83a45b3a9dbe92c1761f49e5f75ed
Drew Fustin <drewfustin@gmail.com>
2015-04-28 20:59:31 -0500

    Added function to generate Google Map html file with isochrone

--- isocronut.py
+++ isocronut.py
@@ -377,3 +377,100 @@ def get_isochrone(origin='',
 
     iso = sort_points(origin, iso, access_type, config_path)
     return iso
+
+
+def generate_isochrone_map(origin='',
+                           duration='',
+                           number_of_angles=12,
+                           tolerance=0.1,
+                           access_type='personal',
+                           config_path='config/'):
+    """
+    Call the get_isochrone function and generate a simple html file using its output.
+    """
+    if origin == '':
+        raise Exception('origin cannot be blank')
+    if duration == '':
+        raise Exception('duration cannot be blank')
+    if not isinstance(number_of_angles, int):
+        raise Exception('number_of_angles must be an int')
+
+    if isinstance(origin, str):
+        origin_geocode = geocode_address(origin, access_type, config_path)
+    elif isinstance(origin, list) and len(origin) == 2:
+        origin_geocode = origin
+    else:
+        raise Exception('origin should be a list [lat, lng] or a string address.')
+
+    iso = get_isochrone(origin, duration, number_of_angles, tolerance, access_type, config_path)
+
+    htmltext = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Isochrone</title>
+    <style>
+      html, body, #map-canvas {{
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }}
+    </style>
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+
+    <script>
+    function initialize() {{
+      var mapOptions = {{
+        zoom: 14,
+        center: new google.maps.LatLng({0},{1}),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      }};
+
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+      var marker = new google.maps.Marker({{
+          position: new google.maps.LatLng({0},{1}),
+          map: map
+      }});
+
+      var isochrone;
+
+      var isochroneCoords = [
+    """.format(origin_geocode[0], origin_geocode[1])
+
+    for i in iso:
+        htmltext += 'new google.maps.LatLng({},{}), \n'.format(i[0], i[1])
+
+    htmltext += """
+      ];
+
+      isochrone = new google.maps.Polygon({
+        paths: isochroneCoords,
+        strokeColor: '#000',
+        strokeOpacity: 0.5,
+        strokeWeight: 1,
+        fillColor: '#000',
+        fillOpacity: 0.25
+      });
+
+      isochrone.setMap(map);
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+
+    </head>
+    <body>
+    <div id="map-canvas"></div>
+    </body>
+    </html>
+    """
+
+    with open('output/isochrone.html', 'w') as f:
+        f.write(htmltext)
+
+    return iso
